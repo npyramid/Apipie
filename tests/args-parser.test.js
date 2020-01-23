7a2807a28ee280ec11f98b129ece42c3563ad4e6
n.pavlov <nikita.pavlov@cashelec.ru>
2020-01-23 20:51:47 +0300

    Update ru docs
    #3

---
+++ tests/args-parser.test.js
@@ -0,0 +1,23 @@
+import parseExecArgs from '../lib/utils/args-parser';
+
+describe('parseExecArgs', () => {
+  const url = '/test/:id';
+
+  test('basic', () => {
+    expect(parseExecArgs(url, { url_params: { id: 1 } }, { _require: { params: false, data: false } })).toEqual({ url: '/test/1' });
+  });
+  test('all', () => {
+    const payload = {
+      url_params: { id: 1 },
+      params: { abc: 'abc' },
+      data: { test: 'test' },
+    };
+    const result = parseExecArgs(url, payload, { _require: { params: false, data: false } });
+    const expected = {
+      url: '/test/1',
+      params: { abc: 'abc' },
+      data: { test: 'test' },
+    };
+    expect(result).toEqual(expected);
+  });
+});
